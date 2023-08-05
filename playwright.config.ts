ad7bbf8438665f8b2bb620f3859fb1fdee577016
lebrancconvas <lebrancconvas@gmail.com>
2023-08-05 21:05:15 +0700

    🔧 chore: update trace and video mode to be on-first-retry

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,8 +27,8 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
-    video: 'on'
+    trace: 'on-first-retry',
+    video: 'on-first-retry'
   },
 
   /* Configure projects for major browsers */
